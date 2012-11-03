5e09001fcce1c4c6306af3617a30897f1c87a059
Kari Hautio <khautio@gmail.com>
2012-11-03 21:34:00 +0200

    cleaning up

--- openLRSng.ino
+++ openLRSng.ino
@@ -43,9 +43,9 @@
 
 //####### TX BOARD TYPE #######
 // tbd. 0 = Original M1 Tx Board
-// tbd. 1 = OpenLRS Rx Board works as TX, reads your PPM signals from first servo port.
-// tbd. 2 = Original M2 Tx Board
-// 3 = OpenLRS Rx v2 Board works as TX, reads your PPM signals from first servo port.
+// tbd. 1 = OpenLRS Rx Board works as TX
+// 2 = Original M2 Tx Board !! NOT TESTED !! Need to add wire between CPU pins 1 and 12!
+// 3 = OpenLRS Rx v2 Board works as TX, servo signal to CH5.
 #define TX_BOARD_TYPE 3
 
 //####### RX BOARD TYPE #######
@@ -53,10 +53,10 @@
 // 3 = OpenLRS Rx v2 Board
 #define RX_BOARD_TYPE 3
 
-//####### BOOSTER (limit maximum power, affects only TX) #######
+//####### BOOSTER (limit maximum power to 50mW, affects only TX) #######
 // 0 = No booster
 // 1 = Booster
-#define BOOSTER 0 // NOT USED !!
+#define BOOSTER 0
 
 //######### Band Select ##########
 // 0 = 433Mhz
@@ -134,7 +134,7 @@ static unsigned char RF_Header[4] = {'@','K','H','a'};
   #define RFM22REG_1E 0x0A
   #define RFM22REG_2A 0x28
 #else
-  #error invalid DATARATE
+  #error Invalid DATARATE
 #endif
 
 #if defined(COMPILE_TX)
@@ -146,128 +146,132 @@ static unsigned char RF_Header[4] = {'@','K','H','a'};
 //####### Board Pinouts #########
 
 #if (BOARD_TYPE == 0)
-    #define PPM_IN A5
-    #define RF_OUT_INDICATOR A4
-    #define BUZZER 9
-    #define BTN 10
-    #define Red_LED 12
-    #define Green_LED 11
+  #error NOT SUPPORTED atm.
+  #define PPM_IN A5
+  #define RF_OUT_INDICATOR A4
+  #define BUZZER 9
+  #define BTN 10
+  #define Red_LED 12
+  #define Green_LED 11
 
-    #define Red_LED_ON  PORTB |= _BV(4);
-    #define Red_LED_OFF  PORTB &= ~_BV(4);
+  #define Red_LED_ON  PORTB |= _BV(4);
+  #define Red_LED_OFF  PORTB &= ~_BV(4);
 
-    #define Green_LED_ON   PORTB |= _BV(3);
-    #define Green_LED_OFF  PORTB &= ~_BV(3);
+  #define Green_LED_ON   PORTB |= _BV(3);
+  #define Green_LED_OFF  PORTB &= ~_BV(3);
 
-    #define PPM_Pin_Interrupt_Setup  PCMSK1 = 0x20;PCICR|=(1<<PCIE1);
-    #define PPM_Signal_Interrupt PCINT1_vect
-    #define PPM_Signal_Edge_Check (PINC & 0x20)==0x20
+  #define PPM_Pin_Interrupt_Setup  PCMSK1 = 0x20;PCICR|=(1<<PCIE1);
+  #define PPM_Signal_Interrupt PCINT1_vect
+  #define PPM_Signal_Edge_Check (PINC & 0x20)==0x20
 
-      //## RFM22B Pinouts for Public Edition (M1 or Rx v1)
-      #define  nIRQ_1 (PIND & 0x08)==0x08 //D3
-      #define  nIRQ_0 (PIND & 0x08)==0x00 //D3
+  //## RFM22B Pinouts for Public Edition (M1 or Rx v1)
+  #define  nIRQ_1 (PIND & 0x08)==0x08 //D3
+  #define  nIRQ_0 (PIND & 0x08)==0x00 //D3
 
-      #define  nSEL_on PORTD |= (1<<4) //D4
-      #define  nSEL_off PORTD &= 0xEF //D4
+  #define  nSEL_on PORTD |= (1<<4) //D4
+  #define  nSEL_off PORTD &= 0xEF //D4
 
-      #define  SCK_on PORTD |= (1<<2) //D2
-      #define  SCK_off PORTD &= 0xFB //D2
+  #define  SCK_on PORTD |= (1<<2) //D2
+  #define  SCK_off PORTD &= 0xFB //D2
 
-      #define  SDI_on PORTC |= (1<<1) //C1
-      #define  SDI_off PORTC &= 0xFD //C1
+  #define  SDI_on PORTC |= (1<<1) //C1
+  #define  SDI_off PORTC &= 0xFD //C1
 
-      #define  SDO_1 (PINC & 0x01) == 0x01 //C0
-      #define  SDO_0 (PINC & 0x01) == 0x00 //C0
+  #define  SDO_1 (PINC & 0x01) == 0x01 //C0
+  #define  SDO_0 (PINC & 0x01) == 0x00 //C0
 
-      #define SDO_pin A0
-      #define SDI_pin A1
-      #define SCLK_pin 2
-      #define IRQ_pin 3
-      #define nSel_pin 4
+  #define SDO_pin A0
+  #define SDI_pin A1
+  #define SCLK_pin 2
+  #define IRQ_pin 3
+  #define nSel_pin 4
 
 #endif
 
 #if (BOARD_TYPE == 1)
-    #define PPM_IN 5
-    #define RF_OUT_INDICATOR 6
-    #define BUZZER 7
-    #define BTN 8
+  #error NOT SUPPORTED atm.
+  #define PPM_IN 5
+  #define RF_OUT_INDICATOR 6
+  #define BUZZER 7
+  #define BTN 8
 
-    #define Red_LED A3
-    #define Green_LED A2
+  #define Red_LED A3
+  #define Green_LED A2
 
-    #define Red_LED_ON  PORTC &= ~_BV(2);PORTC |= _BV(3);
-    #define Red_LED_OFF  PORTC &= ~_BV(2);PORTC &= ~_BV(3);
+  #define Red_LED_ON  PORTC &= ~_BV(2);PORTC |= _BV(3);
+  #define Red_LED_OFF  PORTC &= ~_BV(2);PORTC &= ~_BV(3);
 
-    #define Green_LED_ON  PORTC &= ~_BV(3);PORTC |= _BV(2);
-    #define Green_LED_OFF  PORTC &= ~_BV(3);PORTC &= ~_BV(2);
+  #define Green_LED_ON  PORTC &= ~_BV(3);PORTC |= _BV(2);
+  #define Green_LED_OFF  PORTC &= ~_BV(3);PORTC &= ~_BV(2);
 
-    #define PPM_Pin_Interrupt_Setup  PCMSK2 = 0x20;PCICR|=(1<<PCIE2);
-    #define PPM_Signal_Interrupt PCINT2_vect
-    #define PPM_Signal_Edge_Check (PIND & 0x20)==0x20
+  #define PPM_Pin_Interrupt_Setup  PCMSK2 = 0x20;PCICR|=(1<<PCIE2);
+  #define PPM_Signal_Interrupt PCINT2_vect
+  #define PPM_Signal_Edge_Check (PIND & 0x20)==0x20
 
-      //## RFM22B Pinouts for Public Edition (M1 or Rx v1)
-      #define  nIRQ_1 (PIND & 0x08)==0x08 //D3
-      #define  nIRQ_0 (PIND & 0x08)==0x00 //D3
+  //## RFM22B Pinouts for Public Edition (M1 or Rx v1)
+  #define  nIRQ_1 (PIND & 0x08)==0x08 //D3
+  #define  nIRQ_0 (PIND & 0x08)==0x00 //D3
 
-      #define  nSEL_on PORTD |= (1<<4) //D4
-      #define  nSEL_off PORTD &= 0xEF //D4
+  #define  nSEL_on PORTD |= (1<<4) //D4
+  #define  nSEL_off PORTD &= 0xEF //D4
 
-      #define  SCK_on PORTD |= (1<<2) //D2
-      #define  SCK_off PORTD &= 0xFB //D2
+  #define  SCK_on PORTD |= (1<<2) //D2
+  #define  SCK_off PORTD &= 0xFB //D2
 
-      #define  SDI_on PORTC |= (1<<1) //C1
-      #define  SDI_off PORTC &= 0xFD //C1
+  #define  SDI_on PORTC |= (1<<1) //C1
+  #define  SDI_off PORTC &= 0xFD //C1
 
-      #define  SDO_1 (PINC & 0x01) == 0x01 //C0
-      #define  SDO_0 (PINC & 0x01) == 0x00 //C0
+  #define  SDO_1 (PINC & 0x01) == 0x01 //C0
+  #define  SDO_0 (PINC & 0x01) == 0x00 //C0
 
-      #define SDO_pin A0
-      #define SDI_pin A1
-      #define SCLK_pin 2
-      #define IRQ_pin 3
-      #define nSel_pin 4
+  #define SDO_pin A0
+  #define SDI_pin A1
+  #define SCLK_pin 2
+  #define IRQ_pin 3
+  #define nSel_pin 4
 #endif
 
 #if (BOARD_TYPE == 2)
-    #define PPM_IN 8 // NOTE needs hardware hack to connect D8 and  D3 (CPU pins 1 and 12)
-    #define RF_OUT_INDICATOR A0
-    #define BUZZER 10
-    #define BTN 11
-    #define Red_LED 13
-    #define Green_LED 12
+  #ifndef COMPILE_TX
+  #error TX module cannot be used as RX
+  #endif
 
-    #define Red_LED_ON  PORTB |= _BV(5);
-    #define Red_LED_OFF  PORTB &= ~_BV(5);
+  #define PPM_IN 8 // NOTE needs hardware hack to connect D8 and  D3 (CPU pins 1 and 12)
+  #define RF_OUT_INDICATOR A0
+  #define BUZZER 10
+  #define BTN 11
+  #define Red_LED 13
+  #define Green_LED 12
 
-    #define Green_LED_ON   PORTB |= _BV(4);
-    #define Green_LED_OFF  PORTB &= ~_BV(4);
+  #define Red_LED_ON  PORTB |= _BV(5);
+  #define Red_LED_OFF  PORTB &= ~_BV(5);
 
-    #define PPM_Pin_Interrupt_Setup  PCMSK2 = 0x08;PCICR|=(1<<PCIE2);
-    #define PPM_Signal_Interrupt PCINT2_vect
-    #define PPM_Signal_Edge_Check (PIND & 0x08)==0x08
+  #define Green_LED_ON   PORTB |= _BV(4);
+  #define Green_LED_OFF  PORTB &= ~_BV(4);
 
-      //## RFM22B Pinouts for Public Edition (M2)
-      #define  nIRQ_1 (PIND & 0x04)==0x04 //D2
-      #define  nIRQ_0 (PIND & 0x04)==0x00 //D2
+  //## RFM22B Pinouts for Public Edition (M2)
+  #define  nIRQ_1 (PIND & 0x04)==0x04 //D2
+  #define  nIRQ_0 (PIND & 0x04)==0x00 //D2
 
-      #define  nSEL_on PORTD |= (1<<4) //D4
-      #define  nSEL_off PORTD &= 0xEF //D4
+  #define  nSEL_on PORTD |= (1<<4) //D4
+  #define  nSEL_off PORTD &= 0xEF //D4
 
-      #define  SCK_on PORTD |= (1<<7) //D7
-      #define  SCK_off PORTD &= 0x7F //D7
+  #define  SCK_on PORTD |= (1<<7) //D7
+  #define  SCK_off PORTD &= 0x7F //D7
 
-      #define  SDI_on PORTB |= (1<<0) //B0
-      #define  SDI_off PORTB &= 0xFE //B0
+  #define  SDI_on PORTB |= (1<<0) //B0
+  #define  SDI_off PORTB &= 0xFE //B0
 
-      #define  SDO_1 (PINB & 0x02) == 0x02 //B1
-      #define  SDO_0 (PINB & 0x02) == 0x00 //B1
+  #define  SDO_1 (PINB & 0x02) == 0x02 //B1
+  #define  SDO_0 (PINB & 0x02) == 0x00 //B1
 
-      #define SDO_pin 9
-      #define SDI_pin 8
-      #define SCLK_pin 7
-      #define IRQ_pin 2
-      #define nSel_pin 4
+  #define SDO_pin 9
+  #define SDI_pin 8
+  #define SCLK_pin 7
+  #define IRQ_pin 2
+  #define nSel_pin 4
+
+  #define IRQ_interrupt 0
 #endif
 
 #if (BOARD_TYPE == 3)
@@ -350,8 +354,9 @@ volatile unsigned char tx_buf[11]; // TX buffer
 volatile unsigned char rx_buf[11]; // RX buffer
 
 unsigned char RF_channel = 0;
+
 #define PPM_CHANNELS 8
-volatile int          PPM[PPM_CHANNELS] = { 512,512,512,512,512,512,512,512 };
+volatile int PPM[PPM_CHANNELS] = { 512,512,512,512,512,512,512,512 };
 
 #ifdef COMPILE_TX
 
@@ -444,7 +449,6 @@ void setup() {
   digitalWrite(BUZZER, LOW);
 
   digitalWrite(RF_OUT_INDICATOR,LOW);
-  //digitalWrite(PPM_IN,HIGH);
 
   ppmAge = 255;
   rx_reset();
@@ -581,23 +585,22 @@ volatile unsigned char RF_Mode = 0;
 #define Receive 3
 #define Received 4
 
-unsigned long time;
-unsigned long last_pack_time ;
+unsigned long last_pack_time = 0;
 
-unsigned char RSSI_count = 0;
+unsigned char  RSSI_count = 0;
 unsigned short RSSI_sum = 0;
 
-int ppmCountter=0;
-int ppmTotal=0;
+int ppmCountter = 0;
+int ppmTotal = 0;
 
-unsigned long pwmLastFrame=0;
+unsigned long pwmLastFrame = 0;
 
 boolean PWM_output = 1; // set if parallel PWM output is desired
 
-short FShop,firstpack =0;
-short lostpack =0;
+short firstpack = 0;
+short lostpack = 0;
 
-boolean willhop,fs_saved =0;
+boolean willhop = 0,fs_saved = 0;
 
 void RFM22B_Int()
 {
@@ -614,8 +617,8 @@ void RFM22B_Int()
 ISR(TIMER1_OVF_vect) {
   if (ppmCountter >= PPM_CHANNELS) {
     ICR1 = 40000 - ppmTotal; // 20ms total frame
-    ppmCountter=0;
-    ppmTotal=0;
+    ppmCountter = 0;
+    ppmTotal = 0;
   } else {
     int  ppmOut = 1976 + PPM[ppmCountter++] * 2; // 0-1023 -> 1976 - 4023 (0.988 - 2.012ms)
     ppmTotal += ppmOut;
@@ -657,57 +660,40 @@ struct pwmstep {
 };
 
 void pulsePWM() {
+
   struct pwmstep pwmstep[8];
-  
-  int steps=0;
-  short done=-1;
+  int steps = 0;
+  int done = -1;
   for (int i=0; i<8; i++) {
-    unsigned short smallest=5000;
-    unsigned short mask=0;
+    int smallest = 5000;
+    unsigned short mask = 0;
     for (int j=0; j<8; j++) {
-      if ((PPM[j] > done) && (PPM[j]<smallest)) {
+      if ((PPM[j] > done) && (PPM[j] < smallest)) {
         smallest = PPM[j];
         mask = PWM_MASK[j];
       } else if (PPM[j] == smallest) {
         mask |= PWM_MASK[j];
       }
     }
-    if (smallest!=5000) {
-      done=smallest;
-      pwmstep[steps].mask=mask;
-      pwmstep[steps].time=1976 + smallest * 2;
+    if (smallest != 5000) {
+      done = smallest;
+      pwmstep[steps].mask = mask;
+      pwmstep[steps].time = 1976 + smallest * 2;
       steps++;
     } else {
       break;
     }
   }
-  
-#if 0
-  for (int i=0; i<8; i++) {
-    Serial.print(i);
-    Serial.print('=');
-    Serial.println(PPM[i]);
-  }
-  for (int i=0; i<steps; i++) {
-    Serial.print(i);
-    Serial.print(':');
-    Serial.print(pwmstep[i].time);
-    Serial.print('-');
-    Serial.println(pwmstep[i].mask,HEX);
-  }
-
-  return;
-#endif
 
   cli();
-  int step=0;
-  TCNT1=0;
-  PORTB|=PWM_MASK_PORTB(PWM_ALL_MASK);
-  PORTD|=PWM_MASK_PORTD(PWM_ALL_MASK);
-  while (step<steps) {
-    while (TCNT1<pwmstep[step].time);
-    PORTB&=~PWM_MASK_PORTB(pwmstep[step].mask);
-    PORTD&=~PWM_MASK_PORTD(pwmstep[step].mask);
+  int step = 0;
+  TCNT1 = 0;
+  PORTB |= PWM_MASK_PORTB(PWM_ALL_MASK);
+  PORTD |= PWM_MASK_PORTD(PWM_ALL_MASK);
+  while (step < steps) {
+    while (TCNT1 < pwmstep[step].time);
+    PORTB &= ~PWM_MASK_PORTB(pwmstep[step].mask);
+    PORTD &= ~PWM_MASK_PORTD(pwmstep[step].mask);
     step++;
   }
   sei();
@@ -740,7 +726,7 @@ void load_failsafe_values(void){
   PPM[4]= ee_buf[5] + ((ee_buf[9] & 0x03) << 8);
   PPM[5]= ee_buf[6] + ((ee_buf[9] & 0x0c) << 6);
   PPM[6]= ee_buf[7] + ((ee_buf[9] & 0x30) << 4);
-  PPM[7]= rx_buf[8] + ((rx_buf[9] & 0xc0) << 2);
+  PPM[7]= ee_buf[8] + ((ee_buf[9] & 0xc0) << 2);
 }
 
 void setup() {
@@ -767,14 +753,12 @@ void setup() {
   pinMode(PWM_1,OUTPUT);
   digitalWrite(PWM_1, 1);
   digitalWrite(PWM_2, 1); // enable pullup
-  delay(1000);
+  delay(10);
   if (digitalRead(PWM_2)) {
-    Serial.println("ok1");
     digitalWrite(PWM_1, 0);
-    delay(1000);
+    delay(10);
     if (!digitalRead(PWM_2)) {
       PWM_output=0;
-      Serial.println("PPM activated");
     }
   }
   pinMode(PWM_1,INPUT);
