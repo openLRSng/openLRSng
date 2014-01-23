b60dd4f61a372636d5588cf83fbcb97aef11e94d
Kari Hautio <khautio@gmail.com>
2014-01-23 16:07:21 +0200

    Enable seding of 14 channels on spektrumsat.

--- spektrum.h
+++ spektrum.h
@@ -2,17 +2,20 @@
 #define SPKTRM_SYNC2 0x01
 
 uint32_t spektrumLast = 0;
+uint8_t  spektrumSendHi = 0;
 
 void sendSpektrumFrame()
 {
   uint32_t now = micros();
-  if ((now - spektrumLast) > 20000) {
+  if ((now - spektrumLast) > 10000) {
+    uint8_t  ch = ((spektrumSendHi++) & 1) ? 7 : 0;
     spektrumLast = now;
     Serial.write(SPKTRM_SYNC1);
     Serial.write(SPKTRM_SYNC2);
-    for (uint8_t ch = 0; ch < 7; ch++) {
-      Serial.write((ch<<2) | ((PPM[ch] >> 8) & 0x03));
+    for (uint8_t i = 0; i < 7; i++) {
+      Serial.write((ch << 2) | ((PPM[ch] >> 8) & 0x03));
       Serial.write(PPM[ch] & 0xff);
+      ch++;
     }
   }
 }
