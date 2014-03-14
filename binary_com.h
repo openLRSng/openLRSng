c8b78d620c10b421fb6fb348821c39912afc555f
cTn <ctndesigner@gmail.com>
2014-03-14 17:55:32 +0100

    converting PSP from objective C to C

--- binary_com.h
+++ binary_com.h
@@ -42,408 +42,384 @@ extern uint8_t rxcNumberOfOutputs;
 extern uint16_t rxcVersion;
 uint8_t rxcConnect();
 
+uint8_t PSP_crc;
 
-class binary_PSP
-{
-public:
-  // Constructor
-  binary_PSP() {
-    state = 0;
+void PSP_serialize_uint8(uint8_t data) {
+  Serial.write(data);
+  PSP_crc ^= data;
+}
 
-    payload_length_expected = 0;
-    payload_length_received = 0;
-  };
+void PSP_serialize_uint16(uint16_t data) {
+  PSP_serialize_uint8(lowByte(data));
+  PSP_serialize_uint8(highByte(data));
+}
 
-  void read_packet() {
-    while (Serial.available()) {
-      data = Serial.read();
+void PSP_serialize_uint32(uint32_t data) {
+  for (uint8_t i = 0; i < 4; i++) {
+    PSP_serialize_uint8((uint8_t) (data >> (i * 8)));
+  }
+}
 
-      switch (state) {
-      case 0:
-        if (data == PSP_SYNC1) {
-          state++;
-        }
-        break;
-      case 1:
-        if (data == PSP_SYNC2) {
-          state++;
-        } else {
-          state = 0; // Restart and try again
-        }
-        break;
-      case 2:
-        code = data;
-        message_crc = data;
+void PSP_serialize_uint64(uint64_t data) {
+  for (uint8_t i = 0; i < 8; i++) {
+    PSP_serialize_uint8((uint8_t) (data >> (i * 8)));
+  }
+}
 
-        state++;
-        break;
-      case 3: // LSB
-        payload_length_expected = data;
-        message_crc ^= data;
+void PSP_serialize_float32(float f) {
+  uint8_t *b = (uint8_t*) & f;
 
-        state++;
-        break;
-      case 4: // MSB
-        payload_length_expected |= data << 8;
-        message_crc ^= data;
+  for (uint8_t i = 0; i < sizeof(f); i++) {
+    PSP_serialize_uint8(b[i]);
+  }
+}
 
-        state++;
+void PSP_protocol_head(uint8_t code, uint16_t length) {
+  PSP_crc = 0; // reset crc
 
-        if (payload_length_expected > sizeof(data_buffer)) {
-          // Message too long, we won't accept
-          protocol_head(PSP_INF_DATA_TOO_LONG, 1);
-          serialize_uint8(0x01);
-          protocol_tail();
+  Serial.write(PSP_SYNC1);
+  Serial.write(PSP_SYNC2);
 
-          state = 0; // Restart
-        }
-        break;
-      case 5:
-        data_buffer[payload_length_received] = data;
-        message_crc ^= data;
-        payload_length_received++;
+  PSP_serialize_uint8(code);
+  PSP_serialize_uint16(length);
+}
 
-        if (payload_length_received >= payload_length_expected) {
-          state++;
-        }
-        break;
-      case 6:
-        if (message_crc == data) {
-          // CRC is ok, process data
-          process_data();
-        } else {
-          // respond that CRC failed
-          CRC_FAILED(code, message_crc);
-        }
+void PSP_protocol_tail() {
+  Serial.write(PSP_crc);
+}
 
-        // reset variables
-        memset(data_buffer, 0, sizeof(data_buffer));
+void PSP_ACK() {
+  PSP_protocol_head(PSP_INF_ACK, 1);
 
-        payload_length_received = 0;
-        state = 0;
-        break;
+  PSP_serialize_uint8(0x01);
+}
+
+void PSP_process_data(uint8_t code, uint16_t payload_length_received, uint8_t data_buffer[]) {
+  switch (code) {
+  case PSP_REQ_BIND_DATA:
+    PSP_protocol_head(PSP_REQ_BIND_DATA, sizeof(bind_data));
+    {
+      char* array = (char*) &bind_data;
+      for (uint16_t i = 0; i < sizeof(bind_data); i++) {
+        PSP_serialize_uint8(array[i]);
       }
     }
-  };
+    break;
+  case PSP_REQ_RX_CONFIG:
+    PSP_protocol_head(PSP_REQ_RX_CONFIG, sizeof(rx_config));
+    {
+      char* array = (char*) &rx_config;
+      for (uint16_t i = 0; i < sizeof(rx_config); i++) {
+        PSP_serialize_uint8(array[i]);
+      }
+    }
+    break;
+  case PSP_REQ_RX_JOIN_CONFIGURATION:
+    PSP_protocol_head(PSP_REQ_RX_JOIN_CONFIGURATION, 1);
+    // 1 success, 2 timeout, 3 failed response
 
-  void process_data() {
-    switch (code) {
-    case PSP_REQ_BIND_DATA:
-      protocol_head(PSP_REQ_BIND_DATA, sizeof(bind_data));
-      {
-        char* array = (char*) &bind_data;
-        for (uint16_t i = 0; i < sizeof(bind_data); i++) {
-          serialize_uint8(array[i]);
+    PSP_serialize_uint8(rxcConnect());
+
+    break;
+  case PSP_REQ_SCANNER_MODE:
+    PSP_protocol_head(PSP_REQ_SCANNER_MODE, 1);
+    PSP_serialize_uint8(0x01);
+    PSP_protocol_tail();
+
+    scannerMode();
+
+    return;
+    break;
+  case PSP_REQ_SPECIAL_PINS:
+    PSP_protocol_head(PSP_REQ_SPECIAL_PINS, sizeof(struct rxSpecialPinMap) * rxcSpecialPinCount);
+    {
+      char* array = (char*) &rxcSpecialPins;
+      for (uint16_t i = 0; i < sizeof(struct rxSpecialPinMap) * rxcSpecialPinCount; i++) {
+        PSP_serialize_uint8(array[i]);
+      }
+    }
+    break;
+  case PSP_REQ_FW_VERSION:
+    PSP_protocol_head(PSP_REQ_FW_VERSION, sizeof(version));
+    {
+      PSP_serialize_uint16(version);
+    }
+    break;
+  case PSP_REQ_NUMBER_OF_RX_OUTPUTS:
+    PSP_protocol_head(PSP_REQ_NUMBER_OF_RX_OUTPUTS, 1);
+    {
+      PSP_serialize_uint8(rxcNumberOfOutputs);
+    }
+    break;
+  case PSP_REQ_ACTIVE_PROFILE:
+    PSP_protocol_head(PSP_REQ_ACTIVE_PROFILE, 1);
+    {
+      PSP_serialize_uint8(activeProfile);
+    }
+    break;
+  case PSP_REQ_RX_FAILSAFE: {
+    uint8_t rxtx_buf;
+    rxtx_buf = 'f';
+    tx_packet(&rxtx_buf, 1);
+    rx_reset();
+    RF_Mode = Receive;
+    delay(200);
+
+    if (RF_Mode == Received) {
+      spiSendAddress(0x7f);
+      rxtx_buf = spiReadData();
+      if (rxtx_buf=='F') {
+        PSP_protocol_head(PSP_REQ_RX_FAILSAFE, 32);
+        for (uint8_t i = 0; i < 32; i++) {
+          PSP_serialize_uint8(spiReadData());
         }
+      } else {
+        PSP_protocol_head(PSP_REQ_RX_FAILSAFE, 1);
+        PSP_serialize_uint8(0x01); // failsafe not set
       }
-      break;
-    case PSP_REQ_RX_CONFIG:
-      protocol_head(PSP_REQ_RX_CONFIG, sizeof(rx_config));
-      {
-        char* array = (char*) &rx_config;
-        for (uint16_t i = 0; i < sizeof(rx_config); i++) {
-          serialize_uint8(array[i]);
+    } else {
+      PSP_protocol_head(PSP_REQ_RX_FAILSAFE, 1);
+      PSP_serialize_uint8(0x00); // fail
+    }
+  }
+  break;
+  // SET
+  case PSP_SET_BIND_DATA:
+    PSP_protocol_head(PSP_SET_BIND_DATA, 1);
+
+    if (payload_length_received == sizeof(bind_data)) {
+      char* array = (char*) &bind_data;
+
+      for (uint16_t i = 0; i < sizeof(bind_data); i++) {
+        array[i] = data_buffer[i];
+      }
+
+      PSP_serialize_uint8(0x01);
+    } else {
+      // fail (buffer size doesn't match struct memory size)
+      PSP_serialize_uint8(0x00);
+    }
+    break;
+  case PSP_SET_RX_CONFIG:
+    PSP_protocol_head(PSP_SET_RX_CONFIG, 1);
+
+    if (payload_length_received == sizeof(rx_config)) {
+      char* array = (char*) &rx_config;
+
+      for (uint16_t i = 0; i < sizeof(rx_config); i++) {
+        array[i] = data_buffer[i];
+      }
+
+      PSP_serialize_uint8(0x01);
+    } else {
+      // fail (buffer size doesn't match struct memory size)
+      PSP_serialize_uint8(0x00);
+    }
+    break;
+  case PSP_SET_TX_SAVE_EEPROM:
+    PSP_protocol_head(PSP_SET_TX_SAVE_EEPROM, 1);
+    bindWriteEeprom();
+    PSP_serialize_uint8(0x01); // success
+    break;
+  case PSP_SET_RX_SAVE_EEPROM:
+    PSP_protocol_head(PSP_SET_RX_SAVE_EEPROM, 1);
+    // 1 success, 0 fail
+
+    {
+      uint8_t tx_buf[1 + sizeof(rx_config)];
+      tx_buf[0] = 'u';
+      memcpy(tx_buf + 1, &rx_config, sizeof(rx_config));
+      tx_packet(tx_buf, sizeof(rx_config) + 1);
+      rx_reset();
+      RF_Mode = Receive;
+      delay(200);
+
+      if (RF_Mode == Received) {
+        spiSendAddress(0x7f); // Send the package read command
+        tx_buf[0] = spiReadData();
+        if (tx_buf[0]=='U') {
+          PSP_serialize_uint8(0x01); // success
+        } else {
+          PSP_serialize_uint8(0x00); // fail
         }
+      } else {
+        PSP_serialize_uint8(0x00); // fail
       }
-      break;
-    case PSP_REQ_RX_JOIN_CONFIGURATION:
-      protocol_head(PSP_REQ_RX_JOIN_CONFIGURATION, 1);
-      // 1 success, 2 timeout, 3 failed response
+    }
+    break;
+  case PSP_SET_TX_RESTORE_DEFAULT:
+    PSP_protocol_head(PSP_SET_TX_RESTORE_DEFAULT, 1);
 
-      serialize_uint8(rxcConnect());
+    bindInitDefaults();
 
-      break;
-    case PSP_REQ_SCANNER_MODE:
-      protocol_head(PSP_REQ_SCANNER_MODE, 1);
-      serialize_uint8(0x01);
-      protocol_tail();
+    PSP_serialize_uint8(0x01); // done
+    break;
+  case PSP_SET_RX_RESTORE_DEFAULT:
+    PSP_protocol_head(PSP_SET_RX_RESTORE_DEFAULT, 1);
+    // 1 success, 0 fail
 
-      scannerMode();
+    uint8_t tx_buf[1 + sizeof(rx_config)];
+    tx_buf[0] = 'i';
+    tx_packet(tx_buf,1);
+    rx_reset();
+    RF_Mode = Receive;
+    delay(200);
 
-      return;
-      break;
-    case PSP_REQ_SPECIAL_PINS:
-      protocol_head(PSP_REQ_SPECIAL_PINS, sizeof(struct rxSpecialPinMap) * rxcSpecialPinCount);
-      {
-        char* array = (char*) &rxcSpecialPins;
-        for (uint16_t i = 0; i < sizeof(struct rxSpecialPinMap) * rxcSpecialPinCount; i++) {
-          serialize_uint8(array[i]);
-        }
+    if (RF_Mode == Received) {
+      spiSendAddress(0x7f);   // Send the package read command
+      tx_buf[0] = spiReadData();
+
+      for (uint8_t i = 0; i < sizeof(rx_config); i++) {
+        tx_buf[i + 1] = spiReadData();
       }
-      break;
-    case PSP_REQ_FW_VERSION:
-      protocol_head(PSP_REQ_FW_VERSION, sizeof(version));
-      {
-        serialize_uint16(version);
+
+      memcpy(&rx_config, tx_buf + 1, sizeof(rx_config));
+
+      if (tx_buf[0] == 'I') {
+        PSP_serialize_uint8(0x01); // success
+      } else {
+        PSP_serialize_uint8(0x00); // fail
       }
-      break;
-    case PSP_REQ_NUMBER_OF_RX_OUTPUTS:
-      protocol_head(PSP_REQ_NUMBER_OF_RX_OUTPUTS, 1);
-      {
-        serialize_uint8(rxcNumberOfOutputs);
+    } else {
+      PSP_serialize_uint8(0x00); // fail
+    }
+    break;
+  case PSP_SET_ACTIVE_PROFILE:
+    PSP_protocol_head(PSP_SET_ACTIVE_PROFILE, 1);
+
+    profileSwap(data_buffer[0]);
+    if (!bindReadEeprom()) {
+      bindInitDefaults();
+      bindWriteEeprom();
+    }
+    PSP_serialize_uint8(0x01); // done
+    break;
+  case PSP_SET_RX_FAILSAFE:
+    PSP_protocol_head(PSP_SET_RX_FAILSAFE, 1);
+    {
+      uint8_t rxtx_buf[33];
+      if (payload_length_received == 32) {
+        rxtx_buf[0] = 'g';
+        memcpy(rxtx_buf + 1, data_buffer, 32);
+        tx_packet(rxtx_buf, 33);
+      } else {
+        rxtx_buf[0] = 'G';
+        tx_packet(rxtx_buf, 1);
       }
-      break;
-    case PSP_REQ_ACTIVE_PROFILE:
-      protocol_head(PSP_REQ_ACTIVE_PROFILE, 1);
-      {
-        serialize_uint8(activeProfile);
-      }
-      break;
-    case PSP_REQ_RX_FAILSAFE: {
-      uint8_t rxtx_buf;
-      rxtx_buf = 'f';
-      tx_packet(&rxtx_buf, 1);
       rx_reset();
       RF_Mode = Receive;
       delay(200);
 
       if (RF_Mode == Received) {
         spiSendAddress(0x7f);
-        rxtx_buf = spiReadData();
-        if (rxtx_buf=='F') {
-          protocol_head(PSP_REQ_RX_FAILSAFE, 32);
-          for (uint8_t i = 0; i < 32; i++) {
-            serialize_uint8(spiReadData());
-          }
+        if (spiReadData() == 'G') {
+          PSP_serialize_uint8(0x01);
         } else {
-          protocol_head(PSP_REQ_RX_FAILSAFE, 1);
-          serialize_uint8(0x01); // failsafe not set
+          PSP_serialize_uint8(0x00);
         }
       } else {
-        protocol_head(PSP_REQ_RX_FAILSAFE, 1);
-        serialize_uint8(0x00); // fail
+        PSP_serialize_uint8(0x00);
       }
     }
     break;
-    // SET
-    case PSP_SET_BIND_DATA:
-      protocol_head(PSP_SET_BIND_DATA, 1);
+  case PSP_SET_EXIT:
+    PSP_protocol_head(PSP_SET_EXIT, 1);
+    PSP_serialize_uint8(0x01);
+    PSP_protocol_tail();
 
-      if (payload_length_received == sizeof(bind_data)) {
-        char* array = (char*) &bind_data;
+    binary_mode_active = false;
 
-        for (uint16_t i = 0; i < sizeof(bind_data); i++) {
-          array[i] = data_buffer[i];
-        }
+    return;
+    break;
+  default: // Unrecognized code
+    PSP_protocol_head(PSP_INF_REFUSED, 1);
 
-        serialize_uint8(0x01);
+    PSP_serialize_uint8(0x00);
+  }
+
+  // send over crc
+  PSP_protocol_tail();
+}
+
+void PSP_read(void) {
+  static uint8_t data;
+  static uint8_t state;
+  static uint8_t code;
+  static uint8_t message_crc;
+  static uint16_t payload_length_expected;
+  static uint16_t payload_length_received;
+  static uint8_t data_buffer[100];
+
+  while (Serial.available()) {
+    data = Serial.read();
+
+    switch (state) {
+    case 0:
+      if (data == PSP_SYNC1) {
+        state++;
+      }
+      break;
+    case 1:
+      if (data == PSP_SYNC2) {
+        state++;
       } else {
-        // fail (buffer size doesn't match struct memory size)
-        serialize_uint8(0x00);
+        state = 0; // Restart and try again
       }
       break;
-    case PSP_SET_RX_CONFIG:
-      protocol_head(PSP_SET_RX_CONFIG, 1);
+    case 2:
+      code = data;
+      message_crc = data;
 
-      if (payload_length_received == sizeof(rx_config)) {
-        char* array = (char*) &rx_config;
+      state++;
+      break;
+    case 3: // LSB
+      payload_length_expected = data;
+      message_crc ^= data;
 
-        for (uint16_t i = 0; i < sizeof(rx_config); i++) {
-          array[i] = data_buffer[i];
-        }
+      state++;
+      break;
+    case 4: // MSB
+      payload_length_expected |= data << 8;
+      message_crc ^= data;
 
-        serialize_uint8(0x01);
+      state++;
+
+      if (payload_length_expected > sizeof(data_buffer)) {
+        // Message too long, we won't accept
+        PSP_protocol_head(PSP_INF_DATA_TOO_LONG, 1);
+        PSP_serialize_uint8(0x01);
+        PSP_protocol_tail();
+
+        state = 0; // Restart
+      }
+      break;
+    case 5:
+      data_buffer[payload_length_received] = data;
+      message_crc ^= data;
+      payload_length_received++;
+
+      if (payload_length_received >= payload_length_expected) {
+        state++;
+      }
+      break;
+    case 6:
+      if (message_crc == data) {
+        // CRC is ok, process data
+        PSP_process_data(code, payload_length_received, data_buffer);
       } else {
-        // fail (buffer size doesn't match struct memory size)
-        serialize_uint8(0x00);
+        // respond that CRC failed
+        PSP_protocol_head(PSP_INF_CRC_FAIL, 2);
+
+        PSP_serialize_uint8(code);
+        PSP_serialize_uint8(PSP_crc);
       }
+
+      // reset variables
+      memset(data_buffer, 0, sizeof(data_buffer));
+
+      payload_length_received = 0;
+      state = 0;
       break;
-    case PSP_SET_TX_SAVE_EEPROM:
-      protocol_head(PSP_SET_TX_SAVE_EEPROM, 1);
-      bindWriteEeprom();
-      serialize_uint8(0x01); // success
-      break;
-    case PSP_SET_RX_SAVE_EEPROM:
-      protocol_head(PSP_SET_RX_SAVE_EEPROM, 1);
-      // 1 success, 0 fail
-
-      {
-        uint8_t tx_buf[1 + sizeof(rx_config)];
-        tx_buf[0] = 'u';
-        memcpy(tx_buf + 1, &rx_config, sizeof(rx_config));
-        tx_packet(tx_buf, sizeof(rx_config) + 1);
-        rx_reset();
-        RF_Mode = Receive;
-        delay(200);
-
-        if (RF_Mode == Received) {
-          spiSendAddress(0x7f); // Send the package read command
-          tx_buf[0] = spiReadData();
-          if (tx_buf[0]=='U') {
-            serialize_uint8(0x01); // success
-          } else {
-            serialize_uint8(0x00); // fail
-          }
-        } else {
-          serialize_uint8(0x00); // fail
-        }
-      }
-      break;
-    case PSP_SET_TX_RESTORE_DEFAULT:
-      protocol_head(PSP_SET_TX_RESTORE_DEFAULT, 1);
-
-      bindInitDefaults();
-
-      serialize_uint8(0x01); // done
-      break;
-    case PSP_SET_RX_RESTORE_DEFAULT:
-      protocol_head(PSP_SET_RX_RESTORE_DEFAULT, 1);
-      // 1 success, 0 fail
-
-      uint8_t tx_buf[1 + sizeof(rx_config)];
-      tx_buf[0] = 'i';
-      tx_packet(tx_buf,1);
-      rx_reset();
-      RF_Mode = Receive;
-      delay(200);
-
-      if (RF_Mode == Received) {
-        spiSendAddress(0x7f);   // Send the package read command
-        tx_buf[0] = spiReadData();
-
-        for (uint8_t i = 0; i < sizeof(rx_config); i++) {
-          tx_buf[i + 1] = spiReadData();
-        }
-
-        memcpy(&rx_config, tx_buf + 1, sizeof(rx_config));
-
-        if (tx_buf[0] == 'I') {
-          serialize_uint8(0x01); // success
-        } else {
-          serialize_uint8(0x00); // fail
-        }
-      } else {
-        serialize_uint8(0x00); // fail
-      }
-      break;
-    case PSP_SET_ACTIVE_PROFILE:
-      protocol_head(PSP_SET_ACTIVE_PROFILE, 1);
-
-      profileSwap(data_buffer[0]);
-      if (!bindReadEeprom()) {
-        bindInitDefaults();
-        bindWriteEeprom();
-      }
-      serialize_uint8(0x01); // done
-      break;
-    case PSP_SET_RX_FAILSAFE:
-      protocol_head(PSP_SET_RX_FAILSAFE, 1);
-      {
-        uint8_t rxtx_buf[33];
-        if (payload_length_received == 32) {
-          rxtx_buf[0] = 'g';
-          memcpy(rxtx_buf + 1, data_buffer, 32);
-          tx_packet(rxtx_buf, 33);
-        } else {
-          rxtx_buf[0] = 'G';
-          tx_packet(rxtx_buf, 1);
-        }
-        rx_reset();
-        RF_Mode = Receive;
-        delay(200);
-
-        if (RF_Mode == Received) {
-          spiSendAddress(0x7f);
-          if (spiReadData() == 'G') {
-            serialize_uint8(0x01);
-          } else {
-            serialize_uint8(0x00);
-          }
-        } else {
-          serialize_uint8(0x00);
-        }
-      }
-      break;
-    case PSP_SET_EXIT:
-      protocol_head(PSP_SET_EXIT, 1);
-      serialize_uint8(0x01);
-      protocol_tail();
-
-      binary_mode_active = false;
-
-      return;
-      break;
-    default: // Unrecognized code
-      REFUSED();
     }
-
-    // send over crc
-    protocol_tail();
-  };
-
-  void protocol_head(uint8_t code, uint16_t length) {
-    Serial.write(PSP_SYNC1);
-    Serial.write(PSP_SYNC2);
-
-    crc = 0; // reset crc
-
-    serialize_uint8(code);
-    serialize_uint16(length);
-  };
-
-  void protocol_tail() {
-    Serial.write(crc);
-  };
-
-  void serialize_uint8(uint8_t data) {
-    Serial.write(data);
-    crc ^= data;
-  };
-
-  void serialize_uint16(uint16_t data) {
-    serialize_uint8(lowByte(data));
-    serialize_uint8(highByte(data));
-  };
-
-  void serialize_uint32(uint32_t data) {
-    for (uint8_t i = 0; i < 4; i++) {
-      serialize_uint8((uint8_t) (data >> (i * 8)));
-    }
-  };
-
-  void serialize_uint64(uint64_t data) {
-    for (uint8_t i = 0; i < 8; i++) {
-      serialize_uint8((uint8_t) (data >> (i * 8)));
-    }
-  };
-
-  void serialize_float32(float f) {
-    uint8_t *b = (uint8_t*) & f;
-
-    for (uint8_t i = 0; i < sizeof(f); i++) {
-      serialize_uint8(b[i]);
-    }
-  };
-
-  void ACK() {
-    protocol_head(PSP_INF_ACK, 1);
-
-    serialize_uint8(0x01);
-  };
-
-  void REFUSED() {
-    protocol_head(PSP_INF_REFUSED, 1);
-
-    serialize_uint8(0x00);
-  };
-
-  void CRC_FAILED(uint8_t code, uint8_t failed_crc) {
-    protocol_head(PSP_INF_CRC_FAIL, 2);
-
-    serialize_uint8(code);
-    serialize_uint8(failed_crc);
-  };
-
-private:
-  uint8_t data; // variable used to store a single byte from serial
-
-  uint8_t state;
-  uint8_t code;
-  uint8_t message_crc;
-  uint8_t crc;
-
-  uint16_t payload_length_expected;
-  uint16_t payload_length_received;
-
-  uint8_t data_buffer[100];
-} binary_com;
+  }
+}
