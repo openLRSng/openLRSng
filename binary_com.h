689dd9db96e15b4b595bd6a999733a926a83c5e4
cTn <ctndesigner@gmail.com>
2014-04-09 11:04:23 +0200

    more delay

--- binary_com.h
+++ binary_com.h
@@ -253,7 +253,7 @@ void PSP_process_data(uint8_t code, uint16_t payload_length_received, uint8_t da
       tx_packet(tx_buf, sizeof(rx_config) + 1);
       rx_reset();
       RF_Mode = Receive;
-      delay(600);
+      delay(800);
 
       if (RF_Mode == Received) {
         spiSendAddress(0x7f); // Send the package read command
