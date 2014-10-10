8edb6d82d9d114582103931000c82b8f3013db41
kh4 <khautio@gmail.com>
2014-10-10 10:18:20 +0300

    v3.7.3

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 // Version number in single uint16 [8bit major][4bit][4bit]
 // a.b.c == 0xaabc
-#define OPENLRSNG_VERSION 0x0372
+#define OPENLRSNG_VERSION 0x0373
 static uint16_t version = OPENLRSNG_VERSION;
