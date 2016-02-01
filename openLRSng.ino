8a010b56d2c608d6762b11eda7ac91a0b2eb9ccb
Kari Hautio <khautio@gmail.com>
2016-02-01 19:34:09 +0200

    Update board type list

--- openLRSng.ino
+++ openLRSng.ino
@@ -43,20 +43,22 @@
 // To compile with Arduino select TX/RX and BOARD_TYPE setting as needed below
 
 //####### COMPILATION TARGET #######
-// Enable to compile transmitter code, default is RX (remove leading //)
+// Enable to compile transmitter code, default is RX
 //#define COMPILE_TX 0 // compile RX code
 //#define COMPILE_TX 1 // compile TX code
 
-//####### TX BOARD TYPE #######
-// Enable one of the lines below (remove leading //)
-//#define BOARD_TYPE 0 // 0 = Flytron OpenLRS M1 Tx Board (not verified)
-//#define BOARD_TYPE 1 // 1 = Flytron OpenLRS M1 Rx Board as TX (not verified)
-//#define BOARD_TYPE 2 // 2 = Flytron OpenLRS M2/M3 Tx Board / OrangeRx UHF TX
-//#define BOARD_TYPE 3 // 3 = Flytron OpenLRS Rx v2 Board / OrangeRx UHF RX / HawkEye UHF RX (RX and TX supported)
-//#define BOARD_TYPE 4 // 4 = OpenLRSngTX / HawkEye UHF TX
-//#define BOARD_TYPE 5 // 5 = OpenLRSngRX-4/6ch (DTF UHF/HawkEye) (RX and TX supported)
-//#define BOARD_TYPE 6 // 6 = DTF UHF/HawkEye DeluxeTX (Atmega32u4)
-//#define BOARD_TYPE 9 // 9 = BroversityRX
+//####### BOARD TYPE #######
+// Enable only one of the lines below
+//#define BOARD_TYPE 0 // Flytron OpenLRS M1 Tx Board (not verified)
+//#define BOARD_TYPE 1 // Flytron OpenLRS M1 Rx Board as TX (not verified)
+//#define BOARD_TYPE 2 // Flytron OpenLRS M2/M3 Tx Board / OrangeRx UHF TX
+//#define BOARD_TYPE 3 // Flytron OpenLRS Rx v2 Board / OrangeRx UHF RX / HawkEye UHF RX (RX and TX supported)
+//#define BOARD_TYPE 4 // OpenLRSngTX / HawkEye UHF TX
+//#define BOARD_TYPE 5 // OpenLRSngRX-4/6ch (DTF UHF/HawkEye) (RX and TX supported)
+//#define BOARD_TYPE 6 // DTF UHF/HawkEye DeluxeTX (Atmega32u4)
+//#define BOARD_TYPE 7 // PowerTowerRx
+//#define BOARD_TYPE 8 // openLRSng microRx
+//#define BOARD_TYPE 9 // BroversityRX
 
 //### Module type selection (default = 433, only needed for modified HW)
 //#define RFMTYPE 868
