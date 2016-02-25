69f745e06d669ec79b77f4121a184eadacb502e0
Staffan Sjoqvist <stafan87@gmail.com>
2016-02-25 20:26:33 +0100

    SBUS RX output: Fix correct number of stop bits for SBUS output

--- RX.h
+++ RX.h
@@ -687,8 +687,7 @@ void setup()
       (rx_config.pinMapping[TXD_OUTPUT] == PINMAP_SUMD)) {
     Serial.begin(115200);
   } else if (rx_config.pinMapping[TXD_OUTPUT] == PINMAP_SBUS) {
-    Serial.begin(100000);
-    UCSR0C |= 1<<UPM01; // set even parity
+    Serial.begin(100000, SERIAL_8E2); // Even parity, two stop bits
   } else if ((bind_data.flags & TELEMETRY_MASK) == TELEMETRY_FRSKY) {
     Serial.begin(9600);
   } else {
